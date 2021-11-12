0a6fcb0b8eb2d3f49fbb255ce40b86feec271ea2
James_Calvo <15jcalvo@gmail.com>
2021-11-12 10:33:24 -0500

    Links working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <BrowserRouter>
+        <div>
+        <Link to='/'>Home</Link>
+        </div>
+        <div>
+        <Link id='order-pizza' to='/pizza'>Order Pizza</Link>
+        </div>
+      </BrowserRouter>
     </>
   );
 };
