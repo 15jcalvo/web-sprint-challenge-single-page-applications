560cff2969fc1e3c2563d3265ac2e9f2489bd565
James_Calvo <15jcalvo@gmail.com>
2021-11-12 10:45:59 -0500

    Routing complete with correct rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+function Home(props) {
+  return <h1>Home</h1>
+}
+
+function Pizza(props) {
+  return <h1>Order Form</h1>
+}
 
 const App = () => {
   return (
@@ -7,11 +15,19 @@ const App = () => {
       <h1>Lambda Eats</h1>
       <BrowserRouter>
         <div>
-        <Link to='/'>Home</Link>
+          <Link to='/'>Home</Link>
         </div>
         <div>
-        <Link id='order-pizza' to='/pizza'>Order Pizza</Link>
+          <Link id='order-pizza' to='/pizza'>Order Pizza</Link>
         </div>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/pizza'>
+            <Pizza />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </>
   );
